227828dbf4b5b7818b6e694a6eca8adbc964ed0f
Brayam Corral Flores <brayamcorral@gmail.com>
2022-01-25 19:48:07 -0800

    some css

---
+++ 09-color-generator-bc/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Colors from './Colors'
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <Colors />
+      </div>
+    </>
+  );
+}
+
+export default App;

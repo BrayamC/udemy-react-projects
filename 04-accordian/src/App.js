d51a7c923281910a96c58077c6b811db404c6ed8
Brayam Corral Flores <brayamcorral@gmail.com>
2022-02-01 18:01:33 -0800

    finished video

--- 04-accordian/src/App.js
+++ 04-accordian/src/App.js
@@ -1,8 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  const fetchData = async() => {
+    try {
+      setQuestions(data);
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  return <main>
+      <div className="container">
+        <h3> Questions and answers about login </h3>
+        <section className="info">
+          {
+            questions.map((question) => {
+              return <SingleQuestion key={question.id} {...question}/>
+            })
+
+          }
+        </section>
+      </div>
+
+    </main>
 }
 
 export default App;

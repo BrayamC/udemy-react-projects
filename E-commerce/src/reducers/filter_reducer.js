9512f3cc8c0402e1a5ea1dc5abc23125c8dc81eb
Brayam Corral Flores <brayamcorral@gmail.com>
2022-03-30 13:50:29 -0700

    max price

--- E-commerce/src/reducers/filter_reducer.js
+++ E-commerce/src/reducers/filter_reducer.js
@@ -11,10 +11,13 @@ import {
 
 const filter_reducer = (state, action) => {
   if(action.type === LOAD_PRODUCTS){
+    let maxPrice = action.payload.map((p) => p.price)
+    maxPrice = Math.max(...maxPrice)
     return {
       ...state, 
       all_products:[...action.payload], 
-      filtered_products:[...action.payload]
+      filtered_products:[...action.payload],
+      filters: {...state.filters, max_price: maxPrice, price: maxPrice}
     }
   }
   if(action.type === SET_GRIDVIEW){
@@ -45,6 +48,7 @@ const filter_reducer = (state, action) => {
         return b.name.localeCompare(a.name)
       })
     }
+
 
     return {...state, filtered_products:tempProducts}
   }

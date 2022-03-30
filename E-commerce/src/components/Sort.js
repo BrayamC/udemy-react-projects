db3f58b6339d528d8e5c03c2ea28dfa5aea6e908
Brayam Corral Flores <brayamcorral@gmail.com>
2022-03-29 17:08:19 -0700

    sort component

--- E-commerce/src/components/Sort.js
+++ E-commerce/src/components/Sort.js
@@ -3,7 +3,13 @@ import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 const Sort = () => {
-  const {filtered_products: products, grid_view, setGridView, setListView} = useFilterContext()
+  const {
+    filtered_products: products, 
+    grid_view, setGridView, 
+    setListView,
+    sort,
+    updateSort
+  } = useFilterContext()
 
   return <Wrapper>
       <div className="btn-container">
@@ -18,7 +24,7 @@ const Sort = () => {
       <hr />
       <form>
         <label htmlFor="sort"> sort by </label>
-        <select name="sort" id="" className="sort-input">
+        <select name="sort" id="" className="sort-input" value ={sort} onChange={updateSort}>
           <option value = "price-lowest"> price (lowest) </option>
           <option value = "price-highest"> price (highest) </option>
           <option value = "name-a"> name (a-z) </option>

395477527709280a033297ef6b1052ac5bb3dc60
Brayam Corral Flores <brayamcorral@gmail.com>
2022-04-19 12:40:51 -0700

    usecontect setup

--- E-commerce/src/index.js
+++ E-commerce/src/index.js
@@ -15,13 +15,16 @@ ReactDOM.render(
         domain = {process.env.REACT_APP_AUTH0_DOMAIN}
         clientId= {process.env.REACT_APP_AUTH0_CLIENT_ID}
         redirectUri={window.location.origin}
+        cacheLocation = 'localstorage'
     >
-    <ProductsProvider>
-        <FilterProvider>
-            <CartProvider>
-                <App />
-            </CartProvider>
-        </FilterProvider>
-    </ProductsProvider>
+        <UserProvider>
+            <ProductsProvider>
+                <FilterProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </FilterProvider>
+            </ProductsProvider>
+        </UserProvider>
     </Auth0Provider>, 
 document.getElementById('root'))

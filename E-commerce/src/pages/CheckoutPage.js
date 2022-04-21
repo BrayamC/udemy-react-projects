a44cb66e3745e42f73c69d025d826f825241fb05
Brayam Corral Flores <brayamcorral@gmail.com>
2022-04-21 14:51:28 -0700

    updated logout, empties cart

--- E-commerce/src/pages/CheckoutPage.js
+++ E-commerce/src/pages/CheckoutPage.js
@@ -6,9 +6,25 @@ import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
+
+  const {cart} = useCartContext()
+
   return <main>
     <PageHero title='checkout'/>
-    <Wrapper className = 'page'></Wrapper>
+    <Wrapper className = 'page'>
+      {
+        cart < 1 ? 
+          <div className='empty'>
+            <h2> Your cart is empty </h2>
+            <Link to='/products' className='btn'>
+              fill it
+            </Link>
+          </div> 
+          : 
+          <StripeCheckout />
+      }
+      <h1> checkout here </h1>
+    </Wrapper>
   </main>
 }
 const Wrapper = styled.div``
